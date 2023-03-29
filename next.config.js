2fa28d5636a162e5fb07311d4c390e9cc3063f30
lucesitaliss <ing.liss87@gmail.com>
2023-03-29 14:26:20 -0300

    Add translation

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  i18n: {
+    locales: ['default', 'en-US', 'es-AR', 'pt-br'],
+    defaultLocale: 'default',
+  },
+  trailingSlash: true,
   reactStrictMode: true,
+  localeDetection: false,
 }
 
 module.exports = nextConfig
