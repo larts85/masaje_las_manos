ea5173a44b0b0d2368d5a3dcb091b22fa6d9b71c
lucesitaliss <ing.liss87@gmail.com>
2023-05-23 22:03:17 -0300

    radio button functionality added

--- src/components/storyBook/RadioButtons/index.tsx
+++ src/components/storyBook/RadioButtons/index.tsx
@@ -2,7 +2,7 @@ import 'tailwindcss/tailwind.css'
 import { FC, useState } from 'react'
 export const className = {
   buttonContainer:
-    'w-20 h-92px border-2 border-dashed border-purple-600 flex p-5 m-5',
+    'w-40 h-92px border-2 border-dashed border-purple-600 flex p-5 m-5',
   radioButons:
     'appearance-none w-6 h-6 rounded-full border-2 border-secondary-base absolute cursor-pointer ',
   radioButtonsSelect:
@@ -10,21 +10,43 @@ export const className = {
 }
 
 const RadioButtons: FC = () => {
-  const [isChecked, setIsChecked] = useState(false)
+  const emptyOptions = {
+    option1: '',
+    option2: '',
+  }
+  const [selectOption, setSelectOption] = useState(emptyOptions)
 
-  const handleSubmit = () => {
-    setIsChecked(!isChecked)
+  const handleOptionChange = (option: keyof typeof selectOption) => {
+    setSelectOption(() => ({ ...emptyOptions, [option]: option }))
   }
   return (
     <div className={className.buttonContainer}>
-      <label className="relative m-1 p-1 block">
+      <label className="relative m-3 p-1 block">
         <input
           type="radio"
+          value="option1"
           className={className.radioButons}
-          onClick={handleSubmit}
+          checked={selectOption.option1 === 'option1'}
+          onChange={() => handleOptionChange('option1')}
         />
         <div
-          className={isChecked ? className.radioButtonsSelect : 'hidden'}
+          className={
+            selectOption.option1 ? className.radioButtonsSelect : 'hidden'
+          }
+        ></div>
+      </label>
+      <label className="relative m-3 p-1 block">
+        <input
+          type="radio"
+          value="option2"
+          className={className.radioButons}
+          checked={selectOption.option2 === 'option2'}
+          onChange={() => handleOptionChange('option2')}
+        />
+        <div
+          className={
+            selectOption.option2 ? className.radioButtonsSelect : 'hidden'
+          }
         ></div>
       </label>
     </div>
