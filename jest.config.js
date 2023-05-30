3fb89280b641a1783827f85cc1092af791c9468a
lucesitaliss <ing.liss87@gmail.com>
2023-05-30 12:55:20 -0300

    route bug fix

--- jest.config.js
+++ jest.config.js
@@ -22,8 +22,8 @@ const customJestConfig = {
   moduleNameMapper: {
     '@/(.*)$': '<rootDir>/src/$1',
     '^@/.src/components/(.*)$': '<rootDir>/src/components/$1',
-    '^@/.src/components/storyBook(.*)$':
-      '<rootDir>/src/components/storyBook/$1',
+    '^@/components/storyBook/Typography/Component$':
+      '<rootDir>/src/components/storyBook/Typography/Component',
     '^@/.src/hooks/(.*)$': '<rootDir>/src/hooks/$1',
     '^@/.src/services/(.*)$': '<rootDir>/src/services/$1',
     '^@/.src/pages/(.*)$': '<rootDir>/src/pages/$1',
