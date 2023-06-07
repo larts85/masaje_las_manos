62c7d0ff6577af7d06488bed09b76a5ba490d027
lucesitaliss <ing.liss87@gmail.com>
2023-06-07 10:57:54 -0300

    changes tailwinds.config

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,22 +6,22 @@ import { ClassName, ButtonProps } from './button.types'
 
 export const className: ClassName = {
   [VARIANT.NEUTRAL]: {
-    [VARIANT_STATES.NORMAL]: `w-175px h-52px bg-nautral-base text-nautral-darker rounded-md shadow-predetermined text-${typografyClassName.component.textButton} uppercase
-    hover:bg-nautral-darker hover:text-nautral-light hover:shadow-button-hover active:shadow-button-active active:bg-nautral-darker active:text-nautral-light active:scale-95`,
-    [VARIANT_STATES.DISABLE]: `w-175px h-52px bg-nautral-light text-nautral-base rounded-md text-${typografyClassName.component.textButton} uppercase cursor-not-allowed`,
+    [VARIANT_STATES.NORMAL]: `w-175px h-52px bg-neutral-base text-neutral-darker rounded-md shadow-predetermined text-${typografyClassName.component.textButton} uppercase
+    hover:bg-neutral-darker hover:text-neutral-light hover:shadow-button-hover active:shadow-button-active active:bg-neutral-darker active:text-neutral-light active:scale-95`,
+    [VARIANT_STATES.DISABLE]: `w-175px h-52px bg-neutral-light text-neutral-base rounded-md text-${typografyClassName.component.textButton} uppercase cursor-not-allowed`,
   },
   [VARIANT.PRIMARY]: {
-    [VARIANT_STATES.NORMAL]: `w-175px h-52px bg-primary-base text-nautral-lighter rounded-md text-${typografyClassName.component.textButton} hover:bg-primary-darker hover:text-primary-light hover:shadow-button-primary-hover active:shadow-button-active active:text-primary-light uppercase active:scale-95`,
-    [VARIANT_STATES.DISABLE]: `w-175px h-52px bg-primary-light text-nautral-base rounded-md text-${typografyClassName.component.textButton} uppercase cursor-not-allowed`,
+    [VARIANT_STATES.NORMAL]: `w-175px h-52px bg-primary-base text-neutral-lighter rounded-md text-${typografyClassName.component.textButton} hover:bg-primary-darker hover:text-primary-light hover:shadow-button-primary-hover active:shadow-button-active active:text-primary-light uppercase active:scale-95`,
+    [VARIANT_STATES.DISABLE]: `w-175px h-52px bg-primary-light text-neutral-base rounded-md text-${typografyClassName.component.textButton} uppercase cursor-not-allowed`,
   },
   [VARIANT.SECONDARY]: {
-    [VARIANT_STATES.NORMAL]: `w-175px h-52px bg-secondary-base text-nautral-darker rounded-md shadow-predetermined text-${typografyClassName.component.textButton} uppercase
+    [VARIANT_STATES.NORMAL]: `w-175px h-52px bg-secondary-base text-neutral-darker rounded-md shadow-predetermined text-${typografyClassName.component.textButton} uppercase
     hover:bg-secondary-darker hover:text-secondary-light hover:shadow-button-hover active:shadow-button-active active:bg-secondary-darker active:text-secondary-light active:scale-95`,
-    [VARIANT_STATES.DISABLE]: `w-175px h-52px bg-secondary-light text-nautral-base rounded-md text-${typografyClassName.component.textButton} uppercase cursor-not-allowed`,
+    [VARIANT_STATES.DISABLE]: `w-175px h-52px bg-secondary-light text-neutral-base rounded-md text-${typografyClassName.component.textButton} uppercase cursor-not-allowed`,
   },
   [VARIANT.TRANSPARENT]: {
     [VARIANT_STATES.NORMAL]: `w-175px h-52px cursor-auto text-secondary-base rounded-md  text-${typografyClassName.component.textButton} uppercase hover:shadow-button-hover active:shadow-button-primary-active active:scale-95`,
-    [VARIANT_STATES.DISABLE]: `w-175px h-52px  text-nautral-light rounded-md text-${typografyClassName.component.textButton} uppercase cursor-not-allowed`,
+    [VARIANT_STATES.DISABLE]: `w-175px h-52px  text-neutral-light rounded-md text-${typografyClassName.component.textButton} uppercase cursor-not-allowed`,
   },
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,13 +16,13 @@ module.exports = {
           lighter: '#C1EDEF',
           darker: '#068A8F',
         },
-        soport: {
+        support: {
           error: '#FF4E4E',
           errorLight: '#FF8282',
           success: '#00AF80',
           successLight: '#00E08B',
         },
-        nautral: {
+        neutral: {
           base: '#7B7B7B',
           light: '#C5C5C5',
           lighter: '#F8F8F8',
@@ -34,7 +34,7 @@ module.exports = {
           lighter: '#FFF5E5',
           darker: '#785825',
         },
-        transparency: {
+        transparent: {
           secondaryDarker80: 'rgba(120,88,37,0.8)',
           secondaryBase80: 'rgba(248,212,152,0.8)',
         },
