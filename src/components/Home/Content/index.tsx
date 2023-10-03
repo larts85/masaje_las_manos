099342d17ca53b5d439d2b727eb3d895f9263fc3
lucesitaliss <ing.liss87@gmail.com>
2023-10-03 10:42:50 -0300

    desing responsive HomePage

--- src/components/Home/Content/index.tsx
+++ src/components/Home/Content/index.tsx
@@ -13,7 +13,7 @@ const HomeBoddy: FC = () => {
       <div className="absolute z-10 top-[67vw] right-[0.5vw]">
         <ContentImageOne />
       </div>
-      <p className="absolute z-10 top-[75vw] left-[3vw] w-[205px] font-jost text-right text-[8px] font-normal leading-tight">
+      <p className="absolute z-10 top-[70vw] left-[3vw] w-[65vw] font-jost text-right text-[2.2vw] font-normal leading-tight">
         Bienvenidos a The Hands Massage, donde le brindamos un profesional
         servicio de masaje que puede ser en local y a domicilio, segun su
         preferencia.
@@ -25,17 +25,17 @@ const HomeBoddy: FC = () => {
       <div className="absolute z-10 top-[150vw] left-[5vw]">
         <ContentImageTwo />
       </div>
-      <p className="absolute z-10 top-[130vw] right-[2vw] w-[170px] h-[200px] flex flex-col font-jost text-left text-[11px] font-bold leading-4 ">
+      <p className="absolute z-10 top-[130vw] right-[2vw] w-[55vw] flex flex-col font-jost text-left text-[3.2vw] font-bold leading-4 ">
         ¿Por qué es importante tener una rutina de masaje?
       </p>
-      <p className="absolute z-10 top-[150vw] right-[2vw] w-[170px] h-[200px] flex-shrink-0 font-jost text-left text-[8px] font-normal leading-tight overflow-hidden text-ellipsis">
+      <p className="absolute z-10 top-[150vw] right-[0.1vw] w-[60vw] flex-shrink-0 text-left text-[2.7vw] font-normal leading-tight overflow-hidden text-ellipsis">
         - Mejora la circulación sanguínea. <br />
         - Aumentar la flexibilidad. <br />
         - Mejora la movilidad de las articulaciones. <br />
         - Mejorar el sistema inmunológico. <br />
         - Promueve un mejor sueño. <br />
-        -Reduce la tensión muscular. <br />
-        -Se puede trataar lesiones deportivas.
+        - Reduce la tensión muscular. <br />
+        - Se puede trataar lesiones deportivas.
         <br />
         - Mejora condiciones musculares crónicas. <br />
         - Aliviar dolor e inflamación. <br />- Mejorar la recuperación.
@@ -44,7 +44,7 @@ const HomeBoddy: FC = () => {
       <div className="absolute z-10 top-[253vw] right-[1vw]">
         <ContentImageThree />
       </div>
-      <p className="absolute z-10 top-[230vw] left-[5vw] w-[170px] flex-shrink-0 font-jost text-left text-[8px] font-normal leading-tight">
+      <p className="absolute z-10 top-[230vw] left-[5vw] w-[55vw] flex-shrink-0 font-jost text-left text-[2.5vw] font-normal leading-tight">
         Nuestro equipo de terapeutas se encuentran altamente capacitados y
         listos para brindarle la atención personalizada que usted merece.
         <br /> Lo invitamos a descubrir todos los servicios y locales que
--- src/components/LanguagesButton/languagesButtonStyles.ts
+++ src/components/LanguagesButton/languagesButtonStyles.ts
@@ -5,8 +5,8 @@ const className = {
     'w-max px-[0.5vw] h-max bg-primary-base hover:cursor-pointer rounded-[100px] relative z-10 flex items-center justify-evenly',
   mobileLanguagetext: 'text-secondary-darker font-semibold text-[3vw] p-[2px]',
   languageButtonMenu:
-    'absolute w-full top-0 z-20  transition-opacity duration-1000 ease-in-out',
-  containerLanguageButtonsMenu: 'relative  mx-[1vw]',
+    'absolute w-full top-0 z-20 transition-opacity duration-1000 ease-in-out',
+  containerLanguageButtonsMenu: 'relative mx-[1vw]',
   languageButton:
     'flex flex-wrap w-max h-max gap-[2vw] hover:cursor-pointer bg-primary-base items-center rounded-[100px] relative top-[0px] z-30 justify-around p-[0.8vw]',
   languageText: 'text-secondary-darker font-semibold font-jost text-[2vw]',
