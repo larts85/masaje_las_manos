b37905fa54955ba6179a3fc5fbd21004cdb5b6ec
lucesitaliss <ing.liss87@gmail.com>
2023-12-11 11:42:59 -0300

    responsive

--- src/components/Contact/Form/form.styles.ts
+++ src/components/Contact/Form/form.styles.ts
@@ -1,10 +1,10 @@
 const className = {
-  h2: 'text-[30px] sm:text-[45px] font-splineSans font-black',
-  labels: 'text-[22px] sm:text-[32px] font-splineSans',
+  h2: ' text-[25px] sm:text-[45px] font-splineSans font-black',
+  labels: ' text-[22px] sm:text-[32px] font-splineSans',
   inputs:
-    'w-[500px] h-[71px]  sm:w-[666px] sm:h-[91px] border-[2px] border-[#8E8971] rounded-4px',
+    'w-[200px] h-[61px]  sm:w-[666px] sm:h-[91px] border-[2px] border-[#8E8971] rounded-4px',
   inputMessage:
-    'w-[500px] h-[300px] sm:w-[666px] sm:h-[415px] border-[2px] border-[#8E8971] rounded-4px',
+    'w-[200px] h-[300px] sm:w-[666px] sm:h-[415px] border-[2px] border-[#8E8971] rounded-4px',
 }
 
 export default className
